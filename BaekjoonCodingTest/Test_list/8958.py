bfe6c145fbe6a9ee3f05d7e2000479cbc4044e00
kiddo-creator <krsc0707@gmail.com>
2020-04-09 21:58:11 +0900

    20.04.09 coding test

---
+++ BaekjoonCodingTest/Test_list/8958.py
@@ -0,0 +1,15 @@
+impot sysimport sys
+N = int(sys.stdin.readline())
+list1=[0]*N
+
+for x in list1:
+    x = str(sys.stdin.readline())
+    idx = 0
+    sum1 = 0
+    for a in x:
+        if a == 'O':
+            sum1 += (idx+1)
+            idx += 1
+        else:
+            idx = 0
+    print(sum1)

c1a50de627c5daa572371e52dea1bb9dbc5b68c8
kiddo-creator <krsc0707@gmail.com>
2020-04-21 16:16:51 +0900

    20.04.21 coding test

---
+++ BaekjoonCodingTest/Test_math2/1929.py
@@ -0,0 +1,21 @@
+dw_case, up_case = int(input().split())
+
+#리스트에 넣기
+arr=[]
+for i in range(dw_case,up_case+1):
+    arr.append(i)
+
+#소수판별 후 출력
+for num in arr:
+    if num==1:
+        pass
+    else:
+        temp_num
+        while num<temp_num:
+            if num%temp_num==0:
+                arr.remove(num)
+                pass
+            else:
+                temp_num=temp_num+1
+
+    print(arr[].end="")
--- BaekjoonCodingTest/Test_math2/2581.py
+++ BaekjoonCodingTest/Test_math2/2581.py
@@ -1,25 +1,39 @@
 dn_case = int(input())
 up_case = int(input())
+# 범위설정
 
-arr=[]
+arr=list(range(dn_case,up_case+1))
 
-for x in range(dn_case,up_case):
-    arr.append(x)
-
+#소수판별
 for i in arr:
     temp_num=2
     if i==1:
         del arr[0]
-    if i<temp_num:
-        if i%temp_num==0:
-            del arr[arr.index(i)]
-        else:
-            temp_num=temp_num+1
     else:
-        pass
+        while (temp_num < i):
+            # 자신 보다 작은수로 나눠지면, 소수로 취급하지 않음
+            if (i % temp_num) == 0:
+                print(i)
+                arr.remove(i)
+                break
+            else:
+                temp_num =temp_num + 1
+
+#합 구하기
 sum=0
 for i in arr:
     sum=i+sum
 
-print(sum)
-print(arr.min())
+#최소값 구하기
+min=10000
+for i in arr:
+    if i<min:
+        min=i
+
+#결과 출력
+if sum==0 and min==10000:
+    print(-1)
+else:
+    print(arr)
+    print(sum)
+    print(min)

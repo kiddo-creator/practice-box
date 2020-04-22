56c40834cd742d8fdd99cbea6d7686cd5c9f27e1
kiddo-creator <krsc0707@gmail.com>
2020-04-22 14:46:14 +0900

    2020.04.22 coding test

--- BaekjoonCodingTest/Test_math2/1929.py
+++ BaekjoonCodingTest/Test_math2/1929.py
@@ -1,21 +1,24 @@
-dw_case, up_case = int(input().split())
+import math
+# math import
 
-#리스트에 넣기
-arr=[]
-for i in range(dw_case,up_case+1):
-    arr.append(i)
+dw_case, up_case = map(int,input().split())
+arr=list(range(dw_case,up_case+1))
+new_arr=list()
+#dw_case,up_case arr범위 설정 // new_arr리스트
 
-#소수판별 후 출력
-for num in arr:
-    if num==1:
-        pass
+for i in arr:
+    if i==1: # 1은 소수가 아닙니다.
+        del arr[0]
     else:
-        temp_num
-        while num<temp_num:
-            if num%temp_num==0:
-                arr.remove(num)
-                pass
+        sqrt_i = math.sqrt(i)
+        temp_num = 2
+        while temp_num<=sqrt_i:
+            if i%temp_num==0:
+                new_arr.append(i)
+                break
             else:
                 temp_num=temp_num+1
 
-    print(arr[].end="")
+set(arr)
+set(new_arr)
+print(arr-new_arr)
--- BaekjoonCodingTest/Test_math2/2581.py
+++ BaekjoonCodingTest/Test_math2/2581.py
@@ -1,32 +1,35 @@
 dn_case = int(input())
 up_case = int(input())
-# 범위설정
 
+# 범위설정
 arr=list(range(dn_case,up_case+1))
+new_arr=[]
 
 #소수판별
 for i in arr:
-    temp_num=2
     if i==1:
         del arr[0]
     else:
+        temp_num=2
         while (temp_num < i):
             # 자신 보다 작은수로 나눠지면, 소수로 취급하지 않음
             if (i % temp_num) == 0:
-                print(i)
-                arr.remove(i)
+                new_arr.append(i)
                 break
             else:
                 temp_num =temp_num + 1
 
+#소수 new_arr
+new_arr=set(arr)-set(new_arr)
+
 #합 구하기
 sum=0
-for i in arr:
+for i in new_arr:
     sum=i+sum
 
 #최소값 구하기
 min=10000
-for i in arr:
+for i in new_arr:
     if i<min:
         min=i
 
@@ -34,6 +37,5 @@ for i in arr:
 if sum==0 and min==10000:
     print(-1)
 else:
-    print(arr)
     print(sum)
     print(min)
